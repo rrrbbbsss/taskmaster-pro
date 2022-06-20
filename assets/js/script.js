ea5485e9e123c1d9dcbab043ed06cb61a074b919
Royce Strange <roycest@swbell.net>
2022-06-19 21:28:16 -0500

    finish drag/drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,6 +124,75 @@ $(".list-group").on("blur", "input[type='text']", function () {
   $(this).replaceWith(taskSpan);
 });
 
+//sortable cards
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  dropOnEmpty: true,
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    //console.log("activate", this);
+  },
+  deactivate: function (event) {
+    //console.log("deactivate", this);
+  },
+  over: function (event) {
+    //console.log("over", event.target);
+  },
+  out: function (event) {
+    //console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName =$(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array ont asks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+// delete card
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
